bb18f7487badc646bfdddf2beb026e3c4264d949
Alexandre Negrel <alexandre@negrel.dev>
2025-05-01 20:23:44 +0200

    update nix package

--- flake.nix
+++ flake.nix
@@ -4,22 +4,29 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
     let
       outputsWithoutSystem = { };
-      outputsWithSystem = flake-utils.lib.eachDefaultSystem
-        (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-            };
-            lib = pkgs.lib;
-          in
-          {
-            packages = {
-              default = pkgs.writeScript "log4bash" (builtins.readFile ./lib.sh);
-            };
-          });
+      outputsWithSystem = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          lib = pkgs.lib;
+        in
+        {
+          packages = {
+            default = pkgs.writeScriptBin "log4bash" (builtins.readFile ./lib.sh);
+          };
+        }
+      );
     in
     outputsWithSystem // outputsWithoutSystem;
 }
